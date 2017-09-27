5b1a10ea9b1142d4091b41204baadc06809a09d3
Noelcke <noelcket@oregonstate.edu>
2017-09-27 14:03:45 -0700

    worked on presentation along with adding content to readme.md. Started working on scaffolding for demo.

---
+++ MVCTraining/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Models
+{
+    public class Class
+    {
+        //keeps track of a unique id. Useful later when binding data
+        // and trying to query data.
+        public int ID { get; set; }
+        //Two lets and three digits that identifies what the class is ie: CS 325
+        public string identifier { get; set; } 
+        //The course name
+        public string courseName { get; set; }
+        //who is teaching the class.
+        public string instructor { get; set; }
+    }
+}
---
+++ MVCTraining/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public List<Class> ClastList { get; set; }
+
+    }
+}
