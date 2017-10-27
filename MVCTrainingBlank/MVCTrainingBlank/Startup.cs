fe30ca41d192d2392112058c7c159e9af56af746
Noelcke <noelcket@oregonstate.edu>
2017-10-27 14:01:29 -0700

    working on blank template along with a finished version of the example

---
+++ MVCTrainingBlank/MVCTrainingBlank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTrainingBlank.Startup))]
+namespace MVCTrainingBlank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
